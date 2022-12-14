cdcc3d896bc21cb0dd56169d4099caece470fcb3
Renan Franca <renanfranca.dev@gmail.com>
2022-12-13 22:10:32 -0300

    Adicionado Local Storage e CSS

--- assets/events.js
+++ assets/events.js
@@ -1,12 +1,20 @@
+const $nomeAluno = document.getElementById('name');
+const $nota1 = document.getElementById('n1');
+const $nota2 = document.getElementById('n2');
+const $nota3 = document.getElementById('n3');
+const $nota4 = document.getElementById('n4');
+const $table = document.getElementById('table-body');
+const arrAlunos = getAlunosLocalStorage();
 
-     function addAluno(){
+
+
+     function addAluno(nome, n1, n2, n3, n4, save = 1){
     
-        const $nomeAluno = document.getElementById('name');
-        const $nota1 = document.getElementById('n1');
-        const $nota2 = document.getElementById('n2');
-        const $nota3 = document.getElementById('n3');
-        const $nota4 = document.getElementById('n4');
-        const $table = document.getElementById('table-body');
+    
+        if ($nota1.value > 10 || $nota2.value > 10 || $nota3.value > 10 || $nota4.value > 10 ){
+            alert("Verificar entrada de Notas")
+            return;
+        }
     
     
 
@@ -15,28 +23,29 @@
 
         const alunoNome = document.createElement("td");
         alunoNome.classList.add("aluno-n1")
-        alunoNome.textContent = $nomeAluno.value;
+        alunoNome.textContent = nome || $nomeAluno.value;
         alunoNotas.appendChild(alunoNome);
+
     
 
         const nota1 = document.createElement("td");
         nota1.classList.add("aluno-n1")
-        nota1.textContent = $nota1.value;
+        nota1.textContent = n1 || $nota1.value;
         alunoNotas.appendChild(nota1);
 
         const nota2 = document.createElement("td");
         nota2.classList.add("aluno-n2")
-        nota2.textContent = $nota2.value;
+        nota2.textContent = n2 || $nota2.value;
         alunoNotas.appendChild(nota2);
 
         const nota3 = document.createElement("td");
         nota3.classList.add("aluno-n3")
-        nota3.textContent = $nota3.value;
+        nota3.textContent = n3 || $nota3.value;
         alunoNotas.appendChild(nota3);
 
         const nota4 = document.createElement("td");
         nota4.classList.add("aluno-n4")
-        nota4.textContent = $nota4.value;
+        nota4.textContent = n4 || $nota4.value;
         alunoNotas.appendChild(nota4);
 
         const media = document.createElement("td");
@@ -49,7 +58,22 @@
         resultadoFinal.textContent = "";
         alunoNotas.appendChild(resultadoFinal);
 
+        if(save > 0){
+
+        arrAlunos.push({
+            nome: alunoNome.textContent,
+            nota1: nota1.textContent,
+            nota2: nota2.textContent,
+            nota3: nota3.textContent,
+            nota4: nota4.textContent,
+        });
+    }
+
+        setAlunosData(arrAlunos);
+
         $table.appendChild(alunoNotas);
+
+
     
         select();
         $nomeAluno.value = ""
@@ -59,6 +83,8 @@
         $nota4.value = ""
 
     }
+
+
 
    
     
@@ -72,7 +98,6 @@ function select() {
             var linhaAluno = trs[i];
             var tds = linhaAluno.querySelectorAll('td');
             var notas = agrupaNota(tds);
-            console.log(notas);
             
             var media = calcularMedia.apply(this, notas);
             
@@ -104,9 +129,37 @@ function select() {
            
             return arr;
         }
-
-
 }
+
+//  Local Storage
+function getAlunosLocalStorage (){
+    const alunos = JSON.parse(localStorage.getItem('alunos')) || [];
+    
+    return alunos;
+  };
+
+  function loadAlunos(){
+    const alunos = getAlunosLocalStorage();
+
+    if (alunos.length > 0){
+
+    alunos.forEach(aluno => {
+        addAluno(aluno.nome, aluno.nota1, aluno.nota2, aluno.nota3, aluno.nota4, 0)
+    });
+
+    }
+    return;
+  }
+
+  loadAlunos()
+
+    function setAlunosData(){
+       localStorage.setItem('alunos', JSON.stringify(arrAlunos));
+    }
+    
+
+
+
 
         
        
--- assets/frame.js
+++ assets/frame.js
@@ -4,7 +4,6 @@ function calcularMedia(){
     var qtd = arguments.length;
     var x = 0;
     
-    console.log(arguments.length)
     //while(arguments[x]){
     while(typeof arguments[x] === 'number'){
         
